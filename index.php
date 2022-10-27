0eca106dcef85172a8c451a14235078906d79909
AtonksAlien <losaccoandrea04@gmail.com>
2022-10-27 12:51:03 +0200

    Aggiunti commenti e controlli spazi
    Fixare conta caratteri

--- index.php
+++ index.php
@@ -3,60 +3,114 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body{
+            background-color: #f1f1f1;
+        }
+
+        .container{
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #fff;
+            flex-direction: column;
+        }
+
+        .input{
+            margin: 10px;
+        }
+
+        form{
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            gap: 10px;
+        }
+    </style>
+    <title>ContaCaratteri</title>
 </head>
 <body>
-    <div>
+    <div class="container">
+        <div class="input">
         <form action="index.php" method="post">
             <input type="text" name="stringa">
             <input type="submit" value="Invia">
-    </div>
+        </div>
+        <div class="out">
+            <?php
+            if (isset($_POST["stringa"])) {
+                $stringa = $_POST["stringa"];
 
-    <div class="out">
-        <?php
-        if (isset($_POST["stringa"])) {
-            $stringa = $_POST["stringa"];
-            $vocali = 0;
-            $consonanti = 0;
-            $numeri = 0;
-            $caratteri = 0;
-            $lunghezza = strlen($stringa);
-            $parole = explode(" ", $stringa);
-            $numParole = count($parole);
-            echo "La frase è: " . $stringa . "<br>";
-            echo "La frase è composta da " . $numParole . " parole" . "<br>";
-            for ($i = 0; $i < $lunghezza; $i++) {
-                if (is_numeric($stringa[$i])) {
-                    $numeri++;
-                } elseif (preg_match('/[A-Za-z]/', $stringa[$i])) {
-                    if (preg_match('/[aeiouAEIOU]/', $stringa[$i])) {
-                        $vocali++;
-                    } else {
-                        $consonanti++;
-                    }
-                } elseif ($stringa[$i] != " ") {
-                    $caratteri++;
-                }
-                $count = 0;
-                for ($j = 0; $j < $lunghezza; $j++) {
-                    for ($k = $j; $k > 0 ; $k--) { 
-                        if ($stringa[$i] == $stringa[$j] && $i == 32 && $stringa[$j] = $stringa[$k]) {
-                            $count++;
+                //Cancella spazi superflui
+                $stringa = trim($stringa);
+                $stringa = preg_replace('/\s+/', ' ', $stringa);
+
+                $vocali = 0;
+                $consonanti = 0;
+                $numeri = 0;
+                $caratteri = 0;
+
+                //Ricavo la lunghezza della frase
+                $lunghezza = strlen($stringa);
+
+                //Conto numero parole
+                $parole = explode(" ", $stringa);
+                $numParole = count($parole);
+
+
+                if ($stringa != "") {
+                    echo "La frase è: " . $stringa . "<br>";
+                    echo "La frase è composta da " . $numParole . " parole" . "<br>";
+
+                    //Controllo ogni carattere della frase
+                    for ($i = 0; $i < $lunghezza; $i++) {
+
+                        //Controllo se è un numero
+                        if (is_numeric($stringa[$i])) {
+                            $numeri++;
+
+                            //Controllo se è una lettera
+                        } elseif (preg_match('/[A-Za-z]/', $stringa[$i])) {
+
+                            //Controllo se è una vocale
+                            if (preg_match('/[aeiouAEIOU]/', $stringa[$i])) {
+                                $vocali++;
+                            } else {
+                                $consonanti++;
+                            }
+
+                            //Controllo se è un carattere speciale diverso da uno spazio
+                        } elseif ($stringa[$i] != " ") {
+                            $caratteri++;
                         }
-    
+
+                        //Controllo quanti e quali caratteri sono ripetuti
+                        $conta = substr_count($stringa, $stringa[$i]);
+                        if ($conta > 1) {
+                            echo "Il carattere " . $stringa[$i] . " è ripetuto " . $conta . " volte" . "<br>";
+                        }
+
                     }
-                }
-                if ($count > 1) {
-                    echo $stringa[$i] . " è stato ripetuto " . $count . " volte" . "<br>";
+                    echo "Numero di vocali: " . $vocali . "<br>";
+                    echo "Numero di consonanti: " . $consonanti . "<br>";
+                    echo "Numero di numeri: " . $numeri . "<br>";
+                    echo "Numero di caratteri speciali: " . $caratteri . "<br>";    
+                } else {
+                    echo "Inserisci una frase";
                 }
             }
-            echo "Numero di vocali: " . $vocali . "<br>";
-            echo "Numero di consonanti: " . $consonanti . "<br>";
-            echo "Numero di numeri: " . $numeri . "<br>";
-            echo "Numero di caratteri speciali: " . $caratteri . "<br>";
-        }
-
-        ?>
+    
+            ?>
+        </div>
     </div>
+
 </body>
 </html>
